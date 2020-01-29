84616c91e925fe9d9b7801862775cba8579e7f97
yshmrtm <59944815+yshmrtm@users.noreply.github.com>
2020-01-29 22:15:54 +0900

    first commit

---
+++ modules/iam/main.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "tf_iam_demo" {
+  name = "tf_iam_demo_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "appsync.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
